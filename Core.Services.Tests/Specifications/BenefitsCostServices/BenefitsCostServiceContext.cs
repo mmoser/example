b7912fa16d915ecf4988b7d079f6e02b22fdf3b0
Mike Moser <mike.moser@rivermoss.com>
2018-09-11 09:45:25 -0600

    Removing the values controller. Adding tests around the Benefits Cost Service.

---
+++ Core.Services.Tests/Specifications/BenefitsCostServices/BenefitsCostServiceContext.cs
@@ -0,0 +1,25 @@
+﻿using Core.Model;
+using Core.Services.EmployeeCostPipeline;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Platform.TestHelpers;
+
+namespace Core.Services.Tests.Specifications.BenefitsCostServices
+{
+	public class BenefitsCostServiceContext : SpecificationContext
+	{
+		protected Mock<ILogger<BenefitsCostService>> LoggerMock { get; private set; }
+		protected Mock<IBenefitsRepository> BenefitsRepositoryMock { get; private set; }
+		protected Mock<IEmployeeCostPipe> EmployeeCostPipeMock { get; private set; }
+		protected BenefitsCostService ServiceUnderTest { get; set; }
+
+		protected override void Context()
+		{
+			base.Context();
+			LoggerMock = new Mock<ILogger<BenefitsCostService>>();
+			BenefitsRepositoryMock = new Mock<IBenefitsRepository>();
+			EmployeeCostPipeMock = new Mock<IEmployeeCostPipe>();
+			ServiceUnderTest = new BenefitsCostService(LoggerMock.Object, BenefitsRepositoryMock.Object, EmployeeCostPipeMock.Object);
+		}
+	}
+}
---
+++ Core.Services.Tests/Specifications/BenefitsCostServices/WhenGettingEmployeeCost.cs
@@ -0,0 +1,50 @@
+﻿using Core.Schema.Dtos;
+using Core.Schema.Dtos.BenefitsCost;
+using NUnit.Framework;
+
+namespace Core.Services.Tests.Specifications.BenefitsCostServices
+{
+	public class WhenGettingEmployeeCost : BenefitsCostServiceContext
+	{
+		[Test]
+		public void ShouldGetCompanyCostsFromRepository()
+		{
+			BenefitsRepositoryMock.Verify(x => x.GetCompanyCosts(_companyId));
+		}
+
+		[Test]
+		public void ShouldGetEmployeeCostsFromPipe()
+		{
+			EmployeeCostPipeMock.Verify(x => x.GetEmployeeCost(_employeeDto, _companyCosts));
+		}
+
+		[Test]
+		public void ShouldReturnEmployeeCostThatIsReturnedFromPipe()
+		{
+			Assert.AreEqual(_employeeCost, _result);
+		}
+
+		private EmployeeDto _employeeDto;
+		private EmployeeCostDto _result;
+		private int _companyId;
+		private CompanyCostsDto _companyCosts;
+		private EmployeeCostDto _employeeCost;
+
+		protected override void Because()
+		{
+			_result = ServiceUnderTest.GetEmployeeCost(_companyId, _employeeDto);
+		}
+
+		protected override void Context()
+		{
+			base.Context();
+			_companyId = 1;
+			_employeeDto = new EmployeeDto();
+			_companyCosts = new CompanyCostsDto();
+			_employeeCost = new EmployeeCostDto();
+
+			BenefitsRepositoryMock.Setup(x => x.GetCompanyCosts(_companyId)).Returns(_companyCosts);
+			EmployeeCostPipeMock.Setup(x => x.GetEmployeeCost(_employeeDto, _companyCosts)).Returns(_employeeCost);
+		}
+	}
+}
